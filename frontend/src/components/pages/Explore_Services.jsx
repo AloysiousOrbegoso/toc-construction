8e41e39ab32734ae001307834109d8cce16ec59b
MarcoLucky <luckylunas1117@gmail.com>
2026-08-26 22:01:41 +0800

    compiled all the sections on explore_services.jsx

---
+++ frontend/src/components/pages/Explore_Services.jsx
@@ -0,0 +1,15 @@
+import HeroPage from "../sections/explore_services/heropage";
+import SecondSection from "../sections/explore_services/second_section";
+import ThirdSection from "../sections/explore_services/third_section";
+import FourthSection from "../sections/explore_services/fourth_section";
+
+export default function ExploreServices() {
+	return (
+		<main>
+			<HeroPage />
+			<SecondSection />
+			<ThirdSection />
+			<FourthSection />
+		</main>
+	);
+}
